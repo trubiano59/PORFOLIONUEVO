06fc5f260ad4733be72ed63d297b43ff40feabb3
Miguel Trubiano <trubiano59@hotmail.com>
2022-10-11 12:30:21 -0300

    comienzo incorporación sericios

--- src/app/acercademi/acercademi.component.ts
+++ src/app/acercademi/acercademi.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PorfolioService } from '../servicios/porfolio.service';
 
 @Component({
   selector: 'app-acercademi',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AcercademiComponent implements OnInit {
 
-  constructor() { }
+  constructor(private datosPorfolio:PorfolioService) { }
 
   ngOnInit(): void {
+    this.datosPorfolio.obtenerDatos();
   }
 
 }
