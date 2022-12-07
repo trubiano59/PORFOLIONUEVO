7142bcdfdeb7d0b9a24826f62909ac63da6e6917
Miguel Trubiano <trubiano59@hotmail.com>
2022-12-07 18:26:33 -0300

    arreglo en navbar y json

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './index/index.component';
-import { ErrorComponent } from './error/error.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { ExperienceComponent } from './experience/experience.component';
 import { WorkComponent } from './work/work.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ErrorComponent } from './error/error.component';
 
 const routes: Routes = [
   {path: 'index', component:IndexComponent},
